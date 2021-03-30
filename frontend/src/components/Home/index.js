af28bf6d020fe6cf7e77cc694f3cbb59cb6748ce
Peter Anderson <peterburtonanderson@gmail.com>
2021-03-30 16:12:11 -0600

    header stuff

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Footer from '../Navigation/Footer';
 
 export default function Home() {
 
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 import SignupFormPage from './SignupForm';
 
