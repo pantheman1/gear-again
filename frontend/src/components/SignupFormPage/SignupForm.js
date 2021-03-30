e31ecaa45c167a0e5bc294210b6b531aa88b3568
Peter Anderson <peterburtonanderson@gmail.com>
2021-03-29 21:57:36 -0600

    signup now a modal

---
+++ frontend/src/components/SignupFormPage/SignupForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import './SignupForm.css';
+
+function SignupFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return <Redirect to="/" />;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            setErrors([]);
+            return dispatch(sessionActions.signup({ email, username, password }))
+                .catch(res => {
+                    if (res.data && res.data.errors) setErrors(res.data.errors);
+                });
+        }
+        return setErrors(['Confirm Password field must be the same as the Password field']);
+    };
+
+    return (
+        <>
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <div className="input-label-container">
+                    <label>
+                        Email
+        </label>
+                    <input
+                        className="form__text--input"
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-label-container">
+                    <label>
+                        Username
+        </label>
+                    <input
+                        className="form__text--input"
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-label-container">
+                    <label>
+                        Password
+        </label>
+                    <input
+                        className="form__text--input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="input-label-container">
+                    <label>
+                        Confirm Password
+        </label>
+                    <input
+                        className="form__text--input"
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    );
+}
+
+export default SignupFormPage;
