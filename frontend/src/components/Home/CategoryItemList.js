8e048448efd77fe0144952b416898968ee41438f
Peter Anderson <peterburtonanderson@gmail.com>
2021-04-01 12:13:20 -0600

    Add some seed items. Still need photos.

--- frontend/src/components/Home/CategoryItemList.js
+++ frontend/src/components/Home/CategoryItemList.js
@@ -1,11 +1,16 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function CategoryItemList() {
+    const dispatch = useDispatch();
+
 
 
     return (
         <>
+            <ul>
+
+            </ul>
         </>
     )
 }
