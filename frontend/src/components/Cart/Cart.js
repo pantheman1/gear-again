ffc61076e3471a9194eca34b0117ab739f536afe
Peter Anderson <peterburtonanderson@gmail.com>
2021-04-14 16:28:26 -0600

    Add totals, shipping, tax, etc.

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { CartItem } from "./CartItem";
 import { useSelector } from "react-redux";
 import './Cart.css';
@@ -7,6 +7,8 @@ import './Cart.css';
 export default function Cart() {
     const allItems = useSelector(state => state.items);
     const cart = useSelector(state => state?.cart);
+    const [shipping, setShipping] = useState(5);
+    const [tax, setTax] = useState(0.075);
 
     const cartItems = Object.values(cart);
     // console.log("Cart-------", cart)
@@ -16,12 +18,12 @@ export default function Cart() {
     }
     // debugger
     const filteredItems = Object.values(allItems)?.filter(item => item.id === cart[item.id]?.itemId)
-    const totalPrice = filteredItems?.map(item => {
+    const totalProductPrice = filteredItems?.map(item => {
         return Number(item.price)
     }).reduce((acc, el) => {
         return acc + el;
     })
-    console.log("FILTERED ITEMS", totalPrice)
+    console.log("FILTERED ITEMS", totalProductPrice)
 
     // console.log("CartItemsArr-------", cart)
     if (cartItems.length === 0) {
@@ -32,6 +34,9 @@ export default function Cart() {
             </div>
         )
     }
+
+    const totalTax = Math.ceil(((totalProductPrice * tax) + Number.EPSILON) * 100) / 100;
+    const total = Math.round(((totalProductPrice + shipping + totalTax) + Number.EPSILON) * 100) / 100;
 
     // filter items where item.id === cart[itemId].itemId
 
@@ -49,20 +54,32 @@ export default function Cart() {
                     ))}
                 </div>
                 <div className="checkout__container">
-                    <div className="checkout__container-totals">
-                        <div className="checkout__totals-totalQty">
-                            {totalPrice}
-                        </div>
-                        <div className="checkout__totals-totalShipping">
-
-                        </div>
-                        <div className="checkout__totals-salesTax">
-
-                        </div>
-                        <div className="checkout__totals-totalCost">
-
+                    {/* <div className="checkout__container-totals"> */}
+                    <div className="total__container">
+                        <div className="checkout-label">Total</div>
+                        <div className="total__container-totalProduct">
+                            {`$${totalProductPrice}`}
                         </div>
                     </div>
+                    <div className="shipping__container-productPrice">
+                        <div className="checkout-label">Flat Rate Shipping</div>
+                        <div className="checkout__totals-totalShipping">
+                            {`$${shipping}.00`}
+                        </div>
+                    </div>
+                    <div className="tax__container">
+                        <div className="checkout__totals-label">Sales Tax ({`${tax * 100}%`})</div>
+                        <div className="checkout__totals-salesTax">
+                            {`$${totalTax}`}
+                        </div>
+                    </div>
+                    <div className="total__container">
+                        <div className="checkout__label">Total</div>
+                        <div className="total__container-label">
+                            {`$${total}`}
+                        </div>
+                    </div>
+                    {/* </div> */}
                     <div className="purchase-btn">
                         <button className="form-btn" type="submit" onClick={handlePurchase}>Proceed to Checkout</button>
                     </div>
